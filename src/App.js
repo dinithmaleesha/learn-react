93382da128276821557a73ed453d5732d421fb20
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-07 12:22:07 +0530

    Tutorial #4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welocome to new blog';
+  const like = 50;
+  const person = {name: 'Dinith', age: 22}; // cant out put object and bool
+  const link = 'https://www.google.com';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {like} times</p>
+
+        {/* <p>{ person }</p> */}
+
+        <p>{ 10 }</p>
+        <p>{ 'Hello World' }</p>
+        <p>{ [1, 2, 3, 4, 5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={link}>Google</a>
+      </div>
     </div>
   );
 }
