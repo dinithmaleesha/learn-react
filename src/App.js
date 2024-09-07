579e8a982ce4ee991a621f826920d418f717d39b
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-07 12:31:42 +0530

    Tutorial #5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welocome to new blog';
-  const like = 50;
-  const person = {name: 'Dinith', age: 22}; // cant out put object and bool
-  const link = 'https://www.google.com';
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {like} times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ 'Hello World' }</p>
-        <p>{ [1, 2, 3, 4, 5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google</a>
+        <Home/>
       </div>
     </div>
   );
